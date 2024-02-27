da7d618e534250ccc051fa8f8ce85e7c4e9bfcf5
benlevinNEU <levin.b@northeastern.edu>
2024-02-27 16:44:34 -0500

    Initial commit

---
+++ place-tetris/ai.py
@@ -0,0 +1,411 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from concurrent.futures import ProcessPoolExecutor
+import os, sys, re, copy, time
+from place_tetris import TetrisApp, COLS, ROWS
+import multiprocessing
+from multiprocessing import Process, Lock
+
+import platform
+
+pltfm = None
+if platform.system() == 'Linux' and 'microsoft-standard-WSL2' in platform.release():
+    pltfm = 'WSL'
+    import curses
+    #import keyboard
+else:
+    pltfm = 'Mac'
+    from pynput import keyboard
+    from pynput.keyboard import Key
+    
+import cProfile
+
+# Add the parent directory to sys.path
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+import utils
+from get_latest_profiler_data import print_stats
+import numpy as np
+
+# Use all execpt 1 of the available cores
+MAX_WORKERS = multiprocessing.cpu_count() #- 2 # TODO: Add -1 if you don't want to use all cores
+#MAX_WORKERS = 1 # TODO: Remove this line to use all cores
+PROF_DIR = "./frame-tetris/profiler/"
+
+class TetrisNet(nn.Module):
+    def __init__(self, input_size, hidden_layers, output_size):
+        super(TetrisNet, self).__init__()
+        layers = []
+        previous_layer_size = input_size
+        for hidden_layer_size in hidden_layers:
+            layer = nn.Linear(previous_layer_size, hidden_layer_size)
+            layers.append(layer)
+            layers.append(nn.ReLU())
+            previous_layer_size = hidden_layer_size
+        layers.append(nn.Linear(previous_layer_size, output_size))
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layers(x)
+    
+def load_network(filepath, input_size, hidden_layers, output_size, device='cpu'):
+    """
+    Load a network from a file.
+
+    Args:
+        filepath (str): Path to the .pth file.
+        input_size (int): Size of the input layer.
+        hidden_layers (list): List of sizes of the hidden layers.
+        output_size (int): Size of the output layer.
+        device (str): Device to load the network onto, 'cpu' or 'cuda'.
+
+    Returns:
+        TetrisNet: The loaded network.
+    """
+    network = TetrisNet(input_size, hidden_layers, output_size)
+    state_dict = torch.load(filepath, map_location=device)
+    network.load_state_dict(state_dict)
+    return network
+
+def apply_custom_initialization(m):
+    if isinstance(m, nn.Linear):
+        nn.init.xavier_uniform_(m.weight)
+        nn.init.uniform_(m.bias, -0.01, 0.01)
+
+def evaluate_network(args):
+    """
+    Evaluate the network by playing a game using the provided game function.
+    
+    Args:
+        network (nn.Module): The neural network to evaluate.
+        game_func (callable): A function that takes an action (int) and returns the
+                              flattened game board, score, and game over flag.
+                              
+    Returns:
+        int: The final score achieved by the network.
+    """
+
+    index, network, plays, gp, lock, results_list, slot, profile = args
+
+    if profile[0]:
+        profiler = cProfile.Profile()
+        profiler.enable()
+
+    with torch.no_grad():  # Ensure no gradients are computed during evaluation
+        network.eval()  # Set the network to evaluation mode
+
+        total_score = 0
+
+        width = gp["cell_size"] * (gp["cols"] + 6)
+        height = gp["cell_size"] * gp["rows"] + 80
+        pos = ((width * slot) % 2560, height * int(slot / int(2560 / width)))
+
+        for i in range(plays):
+
+            game = TetrisApp(gui=gp["gui"], cell_size=gp["cell_size"], cols=gp["cols"], rows=gp["rows"], window_pos=pos)
+
+            # Initialize the game
+            board, next_piece = game.get_state()
+            game_over = False
+            
+            move = 0
+            
+            while not game_over and move < 10000: # TODO: Use Simulated anealing to reduce this number to incentivise quicker games
+
+                mask = game.get_possible_states()
+
+                # Flatten the game board and append the next piece
+                flattened_board = np.concatenate((board.flatten(), np.array([next_piece]))).tolist()
+
+                # Convert flattened_board to tensor and add batch dimension
+                board_tensor = torch.tensor(flattened_board, dtype=torch.float32).unsqueeze(0)
+
+                # Forward pass through the network
+                output = np.array(network(board_tensor).squeeze())
+                #print(output)
+                masked_output = output * mask
+                masked_output[masked_output == 0] = -1e9  # Set invalid moves to a very low value
+                #print(masked_output)
+                
+                # Select the action with the highest output value
+                # Adjust this if your network's output does not directly correspond to action indices
+                predicted_action = np.argmax(masked_output)
+                action = predicted_action.item()
+                
+                # Perform the action in the game
+                board, next_piece, score, game_over = game.ai_command(action)
+
+                if gp["gui"]:
+                    #time.sleep(0.01)
+                    pass
+
+                move += 1
+
+            # Accumulate the score over multiple games
+            total_score += score
+
+            #utils.print_to_line(lock, f"Network {index} - Game {i + 1}/{plays} - Final score: {total_score}\n", index)
+
+    if profile[0]:
+        profiler.disable()
+        t = int(time.time())
+        profiler.dump_stats(f"{PROF_DIR}{profile[1]}/proc{index}{t}.prof")
+
+    results_list.append((index, total_score))
+
+def evaluate_population(population, plays, game_params, profile=(False, 0)):
+    manager = multiprocessing.Manager()
+    lock = manager.Lock()
+    results_list = manager.list()  # Managed list for collecting results
+
+    processes = []
+    for i, net in enumerate(population):
+        if i < MAX_WORKERS:
+            slot = i
+
+        if len(processes) >= MAX_WORKERS:
+            # Continuously check if any process has finished
+            while True:
+                # Check each process in the list
+                for p, open_slot in processes[:]:
+                    if not p.is_alive():
+                        processes.remove((p, open_slot))
+                        slot = open_slot
+
+                # Break the loop if we are under the max_workers limit
+                if len(processes) < MAX_WORKERS:
+                    break
+                # Avoid tight loop with a short sleep
+                time.sleep(0.001)
+
+        if MAX_WORKERS > 1:
+            # Start a new process
+            args = (i, net, plays, game_params, lock, results_list, slot, profile)
+            p = multiprocessing.Process(target=evaluate_network, args=(args,))
+            p.start()
+            processes.append((p, slot))
+        else: # If only one worker is available, run the function in the main process
+            evaluate_network((i, net, plays, game_params, lock, results_list, 0, (False, 0)))
+
+        # Simple progress bar
+        total_population = len(population)
+        progress = int((i + 1) / total_population * 100)
+        sys.stdout.write(f"\rGeneration Running: {progress}%")
+        sys.stdout.flush()
+        
+    # Wait for all remaining processes to complete
+    for p, _ in processes:
+        p.join()
+
+    sys.stdout.write(f"\r")
+    sys.stdout.flush()
+
+    # Convert the manager list to a regular list for further processing
+    results = list(results_list)
+    return results
+
+def mutate_network(args):
+    index, network, mutation_rate, mutation_strength, networks_list, profile = args
+
+    if profile[0]:
+        profiler = cProfile.Profile()
+        profiler.enable()
+
+    with torch.no_grad():
+        for param in network.parameters():
+            if len(param.shape) > 1:  # Weights of linear layers
+                for i in range(param.shape[0]):
+                    for j in range(param.shape[1]):
+                        if np.random.rand() < mutation_rate:
+                            param[i][j] += mutation_strength * torch.randn(1).item()  # Use .item()
+            else:  # Biases of linear layers
+                for i in range(param.shape[0]):
+                    if np.random.rand() < mutation_rate:
+                        param[i] += mutation_strength * torch.randn(1).item()  # Use .item()
+
+    if profile[0]:
+        profiler.disable()
+        t = int(time.time())
+        profiler.dump_stats(f"{PROF_DIR}{profile[1]}/proc{index}{t}.prof")
+
+    networks_list.append(network)
+
+def mutate_next_gen(top_networks, population_size, mutation_rate=0.01, mutation_strength=0.1, profile=(False, 0)):
+    next_generation = []
+
+    manager = multiprocessing.Manager()
+    lock = manager.Lock()
+    networks_list = manager.list()  # Managed list for collecting results
+
+    processes = []
+
+    count = 0
+    for net in top_networks:
+        for _ in range(int(population_size / len(top_networks))):
+            count += 1
+
+            if len(processes) >= MAX_WORKERS:
+                # Continuously check if any process has finished
+                while True:
+                    # Check each process in the list
+                    for p in processes:
+                        if not p.is_alive():
+                            processes.remove(p)
+
+                    # Break the loop if we are under the max_workers limit
+                    if len(processes) < MAX_WORKERS:
+                        break
+                    # Avoid tight loop with a short sleep
+                    time.sleep(0.001)
+
+            new_net = copy.deepcopy(net)
+            if MAX_WORKERS > 1:
+                args = (count, new_net, mutation_rate, mutation_strength, networks_list, profile)
+                p = multiprocessing.Process(target=mutate_network, args=(args,))
+                p.start()
+                processes.append(p)
+            else:
+                mutate_network((count, net, mutation_rate, mutation_strength, networks_list, (False, 0)))
+
+            # Simple progress bar
+            progress = int((count) / population_size * 100)
+            sys.stdout.write(f"\rMutating: {progress}%")
+            sys.stdout.flush()
+
+    # Wait for all remaining processes to complete
+    for p in processes:
+        p.join()
+
+    sys.stdout.write(f"\r")
+    sys.stdout.flush()
+
+    next_generation = list(networks_list)
+    return next_generation
+
+def main(stdscr):
+
+    profile = True
+    tid = int(time.time())
+
+    if profile: os.makedirs(f"{PROF_DIR}{tid}")
+
+    if profile:
+        profiler = cProfile.Profile()
+        profiler.enable()
+
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    multiprocessing.set_start_method('spawn')
+
+    # Parameters for the evolutionary process
+    population_size = 50
+    top_n = 8
+    generations = 10000
+    plays = 3  # Number of games to play for each network each generation
+
+    input_size = COLS * ROWS + 1
+    hidden_layers = [400, 200, 100]  # Example hidden layers sizes
+    output_size = 4*COLS  # Adjust based on the number of possible actions
+
+    # Initialize the game
+    game_params = {
+        "gui": False,  # Set to True to visualize the game
+        "cell_size": 20,
+        "cols": COLS,
+        "rows": ROWS,
+        "window_pos": (0, 0)
+    }
+
+    print("Starting Evolutionary Training")
+    #print("\n\n\n\n\n\n\n")
+
+    networks_dir = "./frame-tetris/networks/shape_400_200_100"
+    top_networks_info = utils.find_top_networks(networks_dir, top_n)
+    
+    if len(top_networks_info) == 0:
+        # Initialize population with custom initialization
+        population = [TetrisNet(input_size, hidden_layers, output_size) for _ in range(population_size)]
+        for net in population:
+            net.apply(apply_custom_initialization)
+    else:
+        # Load the top M networks from the file
+        top_networks = [utils.load_network(path, input_size, hidden_layers, output_size, device) for _, path in top_networks_info]
+        population = mutate_next_gen(top_networks, population_size, mutation_rate=0.2, mutation_strength=0.1)
+
+    exit = False
+
+    def on_press(key):
+        global exit
+        # Check if the pressed key is ESC
+        if key == Key.esc:
+            exit = True
+            return False
+
+    # Set up the listener
+    if pltfm == 'Mac':
+        listener = keyboard.Listener(on_press=on_press)
+        listener.start()
+    else:
+        # Initialize curses environment
+        curses.cbreak()  # Disable line buffering
+        stdscr.keypad(True)  # Enable special keys to be recorded
+        curses.noecho()  # Prevent input from being echoed to the screen
+        stdscr.nodelay(True)  # Make getch() non-blocking
+        
+    for generation in range(utils.get_newest_generation_number(networks_dir) + 1, generations):
+
+        # Evaluate all networks in parallel
+        results = evaluate_population(population, plays, game_params, (profile, tid))
+
+        # Convert results to a list and sort by the score
+        results = sorted(results, key=lambda x: x[1], reverse=True)  # Now sorting the list of results
+
+        # Select top performers
+        top_performers = results[:top_n]
+
+        utils.save_networks(networks_dir, [(population[index], score) for index, score in top_performers], generation)
+
+        # Print generation info
+        #sys.stdout.write(f'\033[{population_size+1}B')
+        #sys.stdout.flush()
+        print(f"Generation {generation}: Top Score = {top_performers[0][1]}")
+        #print("\n\n\n\n\n")
+
+        # Extract the networks of the top performers
+        top_networks = [population[index] for index, _ in top_performers]
+
+        population = mutate_next_gen(top_networks, population_size, mutation_rate=0.1, mutation_strength=0.01, profile=(profile, tid))
+
+        if pltfm == 'WSL':
+            # Non-blocking check for input
+            key = stdscr.getch()
+            if key == 27:  # ESC key
+                break
+            elif key != -1:
+                stdscr.refresh()
+
+        # Listen for escape key and break the loop if pressed
+        if exit:
+            break
+
+    print("Finished Evolutionary Training")
+
+    if profile:
+        profiler.disable()
+        profiler.dump_stats(f"{PROF_DIR}{tid}/main.prof")
+
+        profiler_dir = f"{PROF_DIR}{tid}"
+        directory = './frame-tetris'
+
+        p = utils.merge_profile_stats(profiler_dir)
+        print_stats(utils.filter_methods(p, directory).strip_dirs().sort_stats('tottime'))
+        print_stats(p.strip_dirs().sort_stats('tottime'), 30)
+
+if __name__ == "__main__":
+    
+    if pltfm == 'WSL':
+        curses.wrapper(main)
+    else:
+        main(None)
---
+++ place-tetris/place_tetris.py
@@ -0,0 +1,366 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, sys
+# Add the parent directory to sys.path
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+from tetris_utils import *
+import utils
+import cProfile
+from state_gen import get_stone_states
+
+BUFFER_SIZE = 3
+
+all_stone_states = []
+for stone in range(len(tetris_shapes)):
+    all_stone_states.append(get_stone_states(stone))
+
+class TetrisApp(object):
+    def __init__(self, gui=True, cell_size=CELL_SIZE, cols=COLS, rows=ROWS, window_pos=(0, 0)):
+        self.gui = gui
+
+        if gui:
+            os.environ['SDL_VIDEO_WINDOW_POS'] = '{},{}'.format(window_pos[0], window_pos[1])  # Set window position to '0
+            pygame.init()
+            pygame.key.set_repeat(250, 25)
+        self.gui = gui
+        self.cell_size = cell_size
+        self.cols = cols
+        self.rows = rows
+
+        self.width = cell_size * (cols + 6)
+        self.height = cell_size * rows
+        self.rlim = cell_size * cols
+        self.bground_grid = [
+            [8 if x % 2 == y % 2 else 0 for x in range(cols)] for y in range(rows)
+        ]
+
+        if gui:
+            self.default_font = pygame.font.Font(pygame.font.get_default_font(), 12)
+            self.screen = pygame.display.set_mode((self.width, self.height))
+            pygame.event.set_blocked(pygame.MOUSEMOTION)
+        # mouse movement
+        # events, so we
+        # block them.
+        self.next_stoneID = rand(len(tetris_shapes))
+        #self.next_stoneID = 5 # TODO: remove this line
+        self.next_stone = tetris_shapes[self.next_stoneID]
+
+        self.gameover = False
+        self.moves_wo_drop = 0
+        self.max_moves_wo_drop = cols * 2
+
+        self.states = {}
+        self.init_game()
+
+    def remove_row(self, board, row):
+        rows, cols = board.shape
+        # Create a new row of zeros
+        new_row = np.zeros((1, cols), dtype=int)
+        # Concatenate the new row with all rows except the one to remove
+        new_board = np.vstack((new_row, np.delete(board, obj=row, axis=0)))
+        return new_board
+    
+    def new_board(self):
+        board = np.zeros((self.rows, self.cols), dtype=int)
+
+        # Add buffer to bottom of board to avoid index errors
+        buffer = np.ones((BUFFER_SIZE, board.shape[1]), dtype=int)
+        board = np.vstack((buffer, board, buffer))
+        return board
+
+    def new_stone(self):
+        self.stoneID = self.next_stoneID
+        self.stone = self.next_stone[:]
+
+        self.next_stoneID = rand(len(tetris_shapes))
+        #self.next_stoneID = 5 # TODO: remove this line
+        self.next_stone = tetris_shapes[self.next_stoneID]
+
+        self.stone_state = int(self.cols / 2 - len(self.stone[0]) / 2)
+        self.stone_offset = 0
+
+        # Get board state slice for stone
+        self.stone_slice = all_stone_states[self.stoneID][self.stone_state]
+
+        # Drop stone until slice touches top layer of blocks        
+        non_zero_rows = np.all(self.board == 0, axis=1)
+        top_row = len(non_zero_rows) - np.argmax(non_zero_rows[::-1]) - BUFFER_SIZE
+
+        self.stone_offset = top_row - self.stone_slice.shape[0]
+        self.score += self.stone_offset
+
+        if not self.is_valid_state(self.stone_slice, self.stone_offset):
+            self.gameover = True
+
+    def init_game(self):
+        self.board = self.new_board()
+        self.level = 1
+        self.score = 0
+        self.lines = 0
+        self.new_stone()
+
+    def disp_msg(self, msg, topleft):
+        x, y = topleft
+        for line in msg.splitlines():
+            self.screen.blit(
+                self.default_font.render(line, False, (255, 255, 255), (0, 0, 0)),
+                (x, y),
+            )
+            y += 14
+
+    def center_msg(self, msg):
+        for i, line in enumerate(msg.splitlines()):
+            msg_image = self.default_font.render(
+                line, False, (255, 255, 255), (0, 0, 0)
+            )
+
+            msgim_center_x, msgim_center_y = msg_image.get_size()
+            msgim_center_x //= 2
+            msgim_center_y //= 2
+
+            self.screen.blit(
+                msg_image,
+                (
+                    self.width // 2 - msgim_center_x,
+                    self.height // 2 - msgim_center_y + i * 22,
+                ),
+            )
+
+    def draw_matrix(self, matrix, offset):
+        off_x, off_y = offset
+        for y, row in enumerate(matrix):
+            for x, val in enumerate(row):
+
+                if val:
+                    try:
+                        pygame.draw.rect(
+                            self.screen,
+                            colors[val],
+                            pygame.Rect(
+                                (off_x + x) * self.cell_size,
+                                (off_y + y) * self.cell_size,
+                                self.cell_size,
+                                self.cell_size,
+                            ),
+                            0,
+                        )
+                    except IndexError: # TODO: Fix know bug that causes this
+                        with open('./frame-tetris/error-log/log.txt', 'a') as file:
+                            prnt = "\n".join(map(str, matrix))
+                            file.write("New error: \n" + prnt + '\n\n')
+
+    def add_cl_lines(self, n):
+        #linescores = [0, 40, 100, 300, 1200]
+        linescores = [0, 200, 500, 1500, 6000] # TODO: increase reward for lines temporarily
+        self.lines += n
+        self.score += linescores[n] * self.level
+        if self.lines >= self.level * 6:
+            self.level += 1
+
+    def quit(self):
+        self.center_msg("Exiting...")
+        if self.gui: pygame.display.update()
+        sys.exit()
+
+    def drop(self, manual):
+        if not self.gameover:
+            self.score += 1 if manual else 0
+            self.stone_offset += 1
+
+            if not self.is_valid_state(self.stone_slice, self.stone_offset):
+                self.stone_offset -= 1
+                self.board[BUFFER_SIZE+self.stone_offset:BUFFER_SIZE+self.stone_offset+self.stone_slice.shape[0], :] += self.stone_slice
+                cleared_rows = 0
+                while True:
+                    for i, row in enumerate(self.board[BUFFER_SIZE:-BUFFER_SIZE]):
+                        if 0 not in row:
+                            self.board[BUFFER_SIZE:-BUFFER_SIZE] = self.remove_row(self.board[BUFFER_SIZE:-BUFFER_SIZE], i)
+                            cleared_rows += 1
+                            break
+                    else:
+                        break
+                self.add_cl_lines(cleared_rows)
+
+                self.new_stone()
+                return True
+        return False
+
+    def start_game(self):
+        if self.gameover:
+            self.init_game()
+            self.gameover = False
+
+    def update_board(self):
+        self.screen.fill((0, 0, 0))
+        if self.gameover:
+            self.center_msg(
+                """Game Over!\nYour score: %d Press space to continue""" % self.score
+            )
+        else:
+            pygame.draw.line(
+                self.screen,
+                (255, 255, 255),
+                (self.rlim + 1, 0),
+                (self.rlim + 1, self.height - 1),
+            )
+            self.disp_msg("Next:", (self.rlim + self.cell_size, 2))
+            self.disp_msg(
+                "Score: %d\n\nLevel: %d\nLines: %d"
+                % (self.score, self.level, self.lines),
+                (self.rlim + self.cell_size, self.cell_size * 5),
+            )
+            self.draw_matrix(self.bground_grid, (0, 0))
+            self.draw_matrix(self.stone_slice.tolist(), (0, self.stone_offset))
+            self.draw_matrix(self.board[BUFFER_SIZE:-BUFFER_SIZE].tolist(), (0, 0))
+            self.draw_matrix(self.next_stone, (self.cols + 1, 2))
+
+        pygame.display.update()
+
+    # Determines if this state is possible 
+    # Potentially faster replacement for check_collision
+    def is_valid_state(self, stone_slice, offset):
+    
+        board = self.board.copy()
+        stone = stone_slice.copy()
+
+        board[board != 0] = 1
+        stone[stone != 0] = 1
+
+        try:
+            board[offset+BUFFER_SIZE:offset+BUFFER_SIZE+stone.shape[0]] += stone
+        except ValueError:
+            print("Stone: ", stone)
+            print("Offset: ", offset)
+            print("Board: ", board)
+            return False
+
+        if np.any(board > 1):
+            return False
+
+        return True       
+    
+    # Determines if this state is reachable from current state
+    def is_reachable_state(self, stone_slice, offset): # TODO: impliment this
+        return True
+
+    # Rotations in states are represented as rotations from current stone position
+    def get_possible_states(self):
+
+        stone_states = all_stone_states[self.stoneID]
+
+        for i in range(self.cols * 4):
+
+            state = stone_states[i]
+
+            if np.all(state == 0): # Occurs when state index is invalid for stone
+                self.states[i] = (False, state, self.stone_offset)
+                continue
+
+            real = self.is_valid_state(state, self.stone_offset)
+
+            # Determine if this possition is reachable from current state
+            reachable = False
+            if real:
+                reachable = self.is_reachable_state(state, self.stone_offset)
+
+            self.states[i] = (reachable, state, self.stone_offset)
+    
+        keys = np.array(list(self.states.keys()))
+        values = np.array([v[0] for v in self.states.values()])
+
+        # Create an array with zeros
+        mask = np.zeros(4*COLS, dtype=int)
+
+        # Set positions to 1 based on condition
+        mask[keys[values]] = 1
+
+        # For debugging
+        #keys = np.where(mask == 1)[0]
+        #print(keys)
+
+        # Return mask to apply to output vector of ai
+        return mask
+
+    def ai_command(self, state_id):
+
+        # For debugging
+        #print("State ID: ", state_id)
+
+        if self.states[state_id][0] == False:
+            #print("Invalid state ID: ", state_id)
+            sterile_board, _ = self.get_state()
+            return sterile_board, self.next_stoneID, self.score, self.gameover
+
+        state = self.states[state_id]
+
+        # state is (possibility, stone, (x, y)) where possiblity is a bool, and rotation is stored in stone 2D list
+        self.stone_slice = state[1]
+        self.stone_offset = state[2]
+        self.drop(True)
+
+        if self.gui:
+            self.update_board()
+
+        sterile_board, _ = self.get_state()
+        return sterile_board, self.next_stoneID, self.score, self.gameover
+    
+    def get_state(self):
+
+        # Convert to 1s and 2s
+        sterile_board = np.where(self.board != 0, 1, 0)
+        sterile_stone = np.where(self.stone_slice != 0, 2, 0)
+
+        # Add converted stone to board
+        sterile_board[self.stone_offset+BUFFER_SIZE:self.stone_offset+BUFFER_SIZE+sterile_stone.shape[0], :] += sterile_stone
+
+        sterile_board = sterile_board[BUFFER_SIZE:-BUFFER_SIZE]        
+
+        return sterile_board, self.next_stoneID
+
+def print_board(board):
+    sys.stdout.write('\033[F' * len(board))  # Move cursor up to overwrite previous lines
+    for row in board:
+        print(row)
+
+if __name__ == "__main__":
+
+    profiler = cProfile.Profile()
+    profiler.enable()
+
+    App = TetrisApp()
+
+    App.update_board()
+
+    #print("\n"*ROWS)
+
+    while not App.gameover:
+        
+        mask = App.get_possible_states()
+        keys = np.where(mask == 1)[0]
+        print(keys)
+        
+        val = input("Enter selection: ")
+        if val == " ":
+            App.quit()
+            break
+
+        if len(val) <= 2 and val.isdigit():
+            val = int(val)
+        else:
+            print("Invalid input. Please enter a 2-digit number.")
+
+        board, piece, score, gameover = App.ai_command(val)
+
+        print(np.array(board))
+
+        #print_board(board)
+
+    profiler.disable()
+    profiler.dump_stats("./step-tetris/profile_data.prof")
+    stats_file = "./step-tetris/profile_data.prof"
+    directory = './step-tetris/'
+    utils.filter_methods(stats_file, directory)
