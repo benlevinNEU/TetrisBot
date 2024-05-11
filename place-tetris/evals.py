Fixed lineage tracking parent limiters and normalization for polycoef and points

place-tetris/evals.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import product
 from local_params import GP
 
 board = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
@@ -78,6 +79,23 @@ def dfs(matrix, x, y, visited, value, fill=0):
 
     return matrix
 
+def getPolyMax(rows, cols):
+    min_coef = np.inf
+    max_coef = -np.inf
+    
+    for comb in product(range(2), repeat=cols):
+        comb = np.array(comb)
+        comb[comb == 1] = rows
+        poly = np.polyfit(np.arange(cols), comb, 2)[0]
+        if poly < min_coef:
+            min_coef = poly
+            #print(min_coef)
+        if poly > max_coef:
+            max_coef = poly
+            #print(max_coef)
+
+    return min_coef, max_coef
+    
 class Evals():
 
     def __init__(self, GP):
@@ -88,13 +106,18 @@ class Evals():
         self.MX_DTE = GP['cols']/2
         self.MX_BMPS = ((GP['cols']-1)*GP['rows'])
 
-        self.HS = np.zeros(GP['cols'])
+        '''self.HS = np.zeros(GP['cols'])
         self.HS[[0,-1]] = self.MXH
         self.MX_POLY_COEF = np.polyfit(np.arange(GP['cols']), self.HS, 2)[0]
 
         self.LS = np.zeros(GP['cols'])
         self.LS[int(GP['cols']/4):int(np.ceil(GP['cols']*3/4))] = self.MXH
-        self.MN_POLY_COEF = np.polyfit(np.arange(GP['cols']), self.LS, 2)[0]
+        self.MN_POLY_COEF = np.polyfit(np.arange(GP['cols']), self.LS, 2)[0]'''
+
+        '''self.MX_POLY_COEF = self.MXH / (3 ** 2)
+        self.MN_POLY_COEF = -self.MXH / (3 ** 2)'''
+
+        self.MN_POLY_COEF, self.MX_POLY_COEF = getPolyMax(GP['rows'], GP['cols'])
 
         self.POLY_RANGE = self.MX_POLY_COEF - self.MN_POLY_COEF
 
@@ -126,21 +149,18 @@ class Evals():
         max_height = np.max(hs)
         avg_height = np.mean(hs)
 
-        mx_h4e = np.min([np.min(hs), self.GP['cols'] - np.max(hs)])
-        mn_h4e = np.min([np.max(hs), self.GP['cols'] - np.min(hs)])
-
         coef = np.polyfit(np.arange(self.GP['cols']), hs, 2)[0]
 
         n_bmps = bmps / self.MX_BMPS
         n_max_height = max_height / self.MXH
         n_avg_height = avg_height / self.MXH
         n_min_height = min_height / self.MXH
-        n_mx_h4e = mx_h4e / self.MX_DTE
-        n_mn_h4e = mn_h4e / self.MX_DTE
         n_coef = (coef - self.MN_POLY_COEF)/self.POLY_RANGE # Scaling to between 0 and 1 # TODO: Convert old training data
 
         if n_coef < 0:
             n_coef = 0
+        if n_coef > 1:
+            n_coef = 1
 
         fod = min(int(self.MXH - max_height), self.FoD_W) / self.FoD_W
 
@@ -204,7 +224,7 @@ class Evals():
 
         points = cl_pnts + drops
 
-        max_points = 1200 + self.GP['rows'] - 4
+        max_points = 2*(1200 + self.GP['rows']) - 4
 
         return points / max_points
 
@@ -236,6 +256,10 @@ def getEvalLabels():
 
 # Tests
 if __name__ == "__main__":
+
+    min_coef, max_coef = getPolyMax(14, 10)
+    exit()
+
     evals = Evals(GP)
     print(evals.getEvals((board, [])))
 

place-tetris/model.py
@@ -8,6 +8,11 @@ from place_tetris import TetrisApp, trimBoard
 from evals import Evals, NUM_EVALS, getEvalLabels
 import os, multiprocessing
 
+import warnings
+
+# Use this to catch the specific runtime warning
+warnings.filterwarnings('error', category=RuntimeWarning)
+
 from local_params import GP, TP
 
 FT = eval(f"lambda self, x: np.column_stack([{te.decode(TP["feature_transform"])}])")
@@ -108,6 +113,7 @@ class Model():
 
                 else: # Occurs if selected playthrough has no snapshot
                     snapscore = 0
+
                     snapshot = None
                     game = TetrisApp(gui=gp["gui"], cell_size=gp["cell_size"], cols=gp["cols"], rows=gp["rows"], sleep=gp["sleep"], window_pos=pos)
 
@@ -183,8 +189,11 @@ class Model():
                 snap_log.write(f"{trimBoard(snapshot[0])}\n")
                 snap_log.close()
 
-        if self.snapshots[-1][0] != it:
-            self.snapshots.append((it, None))
+        try: # TODO: REMOVE
+            if self.snapshots == [] or self.snapshots[-1][0] != it:
+                self.snapshots.append((it, None))
+        except:
+            pass
 
         # In the event that no more points are scored
         if score - snapscore == 0:
@@ -220,9 +229,14 @@ class Model():
 
     def cost(self, state, tp=TP, ft=FT):
         vals = self.evals.getEvals(state)
-        X = ft(self, vals)
-        if np.isnan(X).any():
-            raise ValueError("X contains NaN values")
+        try:
+            X = ft(self, vals)
+        except:
+            vals[vals>1] = 1
+            vals[vals<0] = 0
+        
+        #if np.isnan(X).any(): # Misfires regularly
+        #    raise ValueError("X contains NaN values")
         costs = X * self.weights
 
         sigma_grad = None